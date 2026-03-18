6420f1f9d3557f4d8cd1422e95c528d50a55ba11
Norbert HERBERT <norbert.herbert@gmail.com>
2026-03-18 14:18:12 +0100

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -84,7 +84,9 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/intro">
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/quick-start/intro">
             Open Documentation
           </Link>
         </div>
